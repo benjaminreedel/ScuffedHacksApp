ed5ca0fafe8c092e75f1ae04c18e37fd0166d8d0
scriche <59908731+scriche@users.noreply.github.com>
2021-05-01 16:21:06 -0700

    TICKRATE WORKING

--- app/src/main/java/com/example/scuffedhacks/UI/TetrisActivity.java
+++ app/src/main/java/com/example/scuffedhacks/UI/TetrisActivity.java
@@ -18,19 +18,28 @@ import com.example.scuffedhacks.Model.Tetrimino;
 import com.example.scuffedhacks.Model.TetrisPlayer;
 import com.example.scuffedhacks.R;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TetrisActivity extends AppCompatActivity {
     private Grid gameGrid;
-
+    private ArrayList<Tetrimino> pieceList;
     ImageView spots[][];
+    private Timer timer = new Timer();
+    private TetrisActivity tetrisActivity;
+    private Random random = new Random();
     public boolean running = true;
-    long tickrate = 2000;
+    long tickrate = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tetris);
-
+        tetrisActivity = TetrisActivity.this;
         gameGrid = new Grid();
+        pieceList = gameGrid.getPieces();
         spots = new ImageView[16][10];
 
         populateGrid();
@@ -40,26 +49,10 @@ public class TetrisActivity extends AppCompatActivity {
         btnLoop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                gameGrid.moveDown(gameGrid.getCurrentPiece());
-//                updateGrid();
-
-                try {
-                    gameloop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+                gameloop();
             }
         });
 
-
-
-
-//        try {
-//            gameloop();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
 
     }
 
@@ -106,13 +99,62 @@ public class TetrisActivity extends AppCompatActivity {
 
     }
 
-    public void gameloop() throws InterruptedException {
-        Log.d("error","banana");
-        for (int i = 0; i < 4; i++) {
-            //Thread.sleep(tickrate);
-            gameGrid.moveDown(gameGrid.getCurrentPiece());
-            updateGrid();
+    public void gameloop() {
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                tetrisActivity.runOnUiThread(new TimerTask() {
+
+                    @Override
+                    public void run() {
+
+                        if(true ) {
+                            gameGrid.moveDown(gameGrid.getCurrentPiece());
+                            updateGrid();
+
+                            if (gameGrid.canMoveDown(gameGrid.getCurrentPiece()) == false) {
+                                int deletedRows = gameGrid.clearRows();
+                                gameGrid.clearRows();
+                                pieceList.remove(gameGrid.getCurrentPiece());
+                                pieceList.add(new Tetrimino(random.nextInt(7) + 1));
+
+                                if (deletedRows> 0) {
+                                    addscore(deletedRows);
+                                }
+
+                                if(true) {
+                                    timer.cancel();
+                                    timer = new Timer();
+                                    gameloop();
+                                }
+                            }
+
+                        }
+                    }
+                });
+            }
+        }, 0, tickrate);
+
+    }
+
+    public int addscore(int lines) {
+        int _score = 0;
+        switch (lines) {
+            case 1:
+                _score += 40;
+                break;
+            case 2:
+                _score += 100;
+                break;
+            case 3:
+                _score += 300;
+                break;
+            case 4:
+                _score += 1200;
+                break;
         }
+        return _score;
     }
 
     private void updateGrid() {
